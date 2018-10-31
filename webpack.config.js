8848cdacb73c880d0afbb09d069613694b387ebd
周羿风 <zwkkkk1@163.com>
2018-10-31 17:16:27 +0800

    chapter 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
+var path = require('path')
+
 module.exports = {
-  entry: './index.js',
+  // 入口 入口文件是 ./src/index.js
+  entry: path.join(__dirname, './src/index.js'),
+  // 出口 文件会输出到 dist 文件夹，输出的文件名叫 bundle.js
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        use: ['babel-loader?cacheDirectory=true'],
+        include: path.join(__dirname, 'src')
       }
     ]
+  },
+
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true,
   }
 }
