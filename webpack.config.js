5d95d2b282dc2bd4b2822f917a7e07349cf43e30
周羿风 <zwkkkk1@163.com>
2019-03-21 13:24:50 +0800

    fix

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path')
+
+module.exports = {
+  // 入口 入口文件是 ./app/app.js
+  entry: path.join(__dirname, './app/index.js'),
+  // 出口 文件会输出到 dist 文件夹，输出的文件名叫 bundle.js
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader?cacheDirectory=true'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true,
+  },
+}
